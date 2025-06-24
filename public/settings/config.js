dabaaa2f97ac18526fe14e1acb12d5a90a025ae9
kiuur <kyuurzy@gmail.com>
2025-06-24 19:02:51 +0800

    ngapain njr?

---
+++ public/settings/config.js
@@ -0,0 +1,4 @@
+global.creator = "@kyuubeyours - tester"
+global.token = "7872525280:AAGDa8y2LeldVoUaj8aHvdMZhQTweB3WJ_c"
+global.chatid = "7889227094"
+global.watermark = "© kyuubeyours — log notification"
